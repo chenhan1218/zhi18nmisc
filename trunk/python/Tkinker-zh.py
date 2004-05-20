db654ffabc5fa0cf70d47c9abe9c88041eb0d2ad
ycheng <ycheng@a49f72c6-99e5-0310-b996-e912c5eb9f26>
2004-05-20 03:51:35 +0000

    Chinese Tkinker python sample code.
    git-svn-id: https://www.openfoundry.org/svn/zhi18nmisc@5 a49f72c6-99e5-0310-b996-e912c5eb9f26

---
+++ trunk/python/Tkinker-zh.py
@@ -0,0 +1,9 @@
+#
+# Some one say this can let's you have Chinese python window.
+# (I think this is MS Windows python)
+#
+from Tkinter import *
+w = Button(text="«¢Åo¡I".decode("mbcs"), font="²Ó©úÅé", command='exit')
+w.pack()
+w.mainloop()
+
